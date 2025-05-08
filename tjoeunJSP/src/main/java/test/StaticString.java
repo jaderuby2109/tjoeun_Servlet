bbab945748c1fd3ca5e945fe6d31cf1ff42eee9b
jaderuby2109 <jaderuby2109@gmail.com>
2025-05-08 19:23:53 +0900

    동적 아닌거 삭제

--- tjoeunJSP/src/main/java/test/StaticString.java
+++
@@ -1,7 +0,0 @@
-package test;
-
-public class StaticString {
-	public static final String UTF_8 = "UTF-8";
-	public static final String TEXT_PLAIN = "text/plain; charset=UTF-8";
-	public static final String TEXT_HTML = "text/html; charset=UTF-8";
-}
