ba9a20a360c55368fa6b86361bc5e6452b787a91
jaderuby2109 <jaderuby2109@gmail.com>
2025-05-09 18:10:20 +0900

    Delete tjouenServlet directory

--- tjouenServlet/src/main/java/test/StaticString.java
+++
@@ -1,9 +0,0 @@
-package test;
-
-public class StaticString {
-	public static final String UTF_8 = "UTF-8";
-	public static final String TEXT_PLAIN = "text/plain; charset=UTF-8";
-	public static final String TEXT_HTML = "text/html; charset=UTF-8";
-	public static final String JSON = "application/json; charset=UTF-8";
-	public static final String OCTET_STREAM = "application/octet-stream; charset=UTF-8";
-}
